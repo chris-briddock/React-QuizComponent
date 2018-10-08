1a729cc333c941390081c229e94da0bdda35777a
Christopher Briddock <cbriddock@transactorgsl.com>
2018-10-08 13:57:33 +0100

    Pluralsight

--- src/Quiz.js
+++ src/Quiz.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 let quizData = require('./quiz_data.json')
 
-export default class Quiz extends Component {
+class Quiz extends Component {
     constructor(props){
         super(props);
         this.state = { quiz_position: 1 }
@@ -15,3 +15,5 @@ export default class Quiz extends Component {
         </div>
     }
 }
+
+export default Quiz;
